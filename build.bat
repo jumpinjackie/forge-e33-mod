33a4a6b5d375b200a679c2d6757eaff8b9ac9e20
Jackie Ng <jumpinjackie@gmail.com>
2025-09-18 19:44:48 +1000

    Clear target files/dirs first

--- build.bat
+++ build.bat
@@ -1,5 +1,10 @@
 if not exist "%APPDATA%\Forge" mkdir "%APPDATA%\Forge"
 if not exist "%APPDATA%\Forge\custom" mkdir "%APPDATA%\Forge\custom"
+
+rd /s /q "%APPDATA%\Forge\custom\cards"
+rd /s /q "%APPDATA%\Forge\custom\editions" 
+rd /s /q "%APPDATA%\Forge\custom\tokens"
+
 if not exist "%APPDATA%\Forge\custom\cards" mkdir "%APPDATA%\Forge\custom\cards"
 if not exist "%APPDATA%\Forge\custom\editions" mkdir "%APPDATA%\Forge\custom\editions"
 if not exist "%APPDATA%\Forge\custom\tokens" mkdir "%APPDATA%\Forge\custom\tokens"
