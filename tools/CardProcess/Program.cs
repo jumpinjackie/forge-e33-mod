8c2d7d67e011c53894ee65dfec5dbd83e2a9080a
Jackie Ng <jumpinjackie@gmail.com>
2025-12-12 00:23:01 +1100

    #2: Update SPOILER.md generation to use thumbnail images instead of full sized ones.

--- tools/CardProcess/Program.cs
+++ tools/CardProcess/Program.cs
@@ -1284,7 +1284,7 @@ public class GenAllCommand : BaseCommand
         // Generate SPOILER.md containing a 3-column table of card and token images
         try
         {
-            var picsBase = Path.Combine(this.BaseDirectory.FullName, "pics");
+            var picsBase = Path.Combine(this.BaseDirectory.FullName, "thumbs");
             var baseCardsPicsDir = Path.Combine(picsBase, "cards", "E33");
             var cmdrCardsPicsDir = Path.Combine(picsBase, "cards", "E3C");
             var tokensPicsDir = Path.Combine(picsBase, "tokens", "E33");
